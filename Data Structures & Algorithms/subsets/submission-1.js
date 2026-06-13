f8d25b332ac6dc741fa9c3ab903e93137c29269f
Jithil P Ponnan <MrJithil@users.noreply.github.com>
2026-06-13 23:19:58 +1000

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    subsets(nums) {
+        const results = [];
+        const subset = [];
+
+        const dfs = (i)=>{
+            if(i >= nums.length){
+                results.push([...subset]);
+                return;
+            }
+
+            subset.push(nums[i]);
+            dfs(i + 1);
+            subset.pop();
+            dfs(i + 1);
+        }
+
+        dfs(0);
+        return results;
+    }
+}
